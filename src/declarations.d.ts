05ebfb37602fdb6ec5ea7d919a9d37dbb72502d4
Hermann Hahn <hermann.h.hahn@gmail.com>
2026-02-23 21:55:13 -0300

    Deploy N. 92

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -5,6 +5,17 @@ interface RoomMemory {
     primaryHostileTargetId?: Id<Creep> | null; // Adicionado para o foco de fogo das torres
 }
 
+interface CreepMemory {
+    role: string;
+    room?: string;
+    working?: boolean;
+    sourceId?: Id<Source>;
+    targetEnergyId?: Id<any>;
+    deliveryTargetId?: Id<any>;
+    assignedSupplier?: Id<Creep>;
+    delivering?: boolean;
+}
+
 // Global Memory
 interface Memory {
     uuid: number;
