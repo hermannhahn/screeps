891e9cbc179c292f0359666a13e02af6f11c5abb
Hermann Hahn <hermann.h.hahn@gmail.com>
2026-03-06 19:15:34 -0300

    Main: restore full civilization script after total purification

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import RoomScanner from "./rooms/room.scanner";
-// import RoomPlanner from "./rooms/room.planner";
-// import SpawnLogic from "./spawns/spawn.logic";
-// import ToolPixel from "./tools/tool.pixel";
-// import RoleHarvester from "./roles/role.harvester";
-// import RoleSupplier from "./roles/role.supplier";
-// import RoleUpgrader from "./roles/role.upgrader";
-// import RoleWorker from "./roles/role.worker";
+import RoomPlanner from "./rooms/room.planner";
+import SpawnLogic from "./spawns/spawn.logic";
+import ToolPixel from "./tools/tool.pixel";
+import RoleHarvester from "./roles/role.harvester";
+import RoleSupplier from "./roles/role.supplier";
+import RoleUpgrader from "./roles/role.upgrader";
+import RoleWorker from "./roles/role.worker";
 
 export const loop = () => {
   // Clear dead creep memory
@@ -18,32 +18,16 @@ export const loop = () => {
   // Run Room Scanner
   RoomScanner.run();
 
-  // PURIFICATION PROTOCOL: Destroy EVERYTHING not planned (high frequency)
-  for (const roomName in Game.rooms) {
-    const room = Game.rooms[roomName];
-    if (room.controller && room.controller.my) {
-      // 1. Destroy all construction sites
-      const sites = room.find(FIND_CONSTRUCTION_SITES);
-      for (const site of sites) site.remove();
+  // Run Room Planner
+  RoomPlanner.run();
 
-      // 2. Destroy all structures except Spawn/Controller
-      const structures = room.find(FIND_STRUCTURES, {
-        filter: (s) => s.structureType !== STRUCTURE_SPAWN && s.structureType !== STRUCTURE_CONTROLLER
-      });
-      for (const s of structures) {
-        console.log(`[Purification] Annhilating ${s.structureType} at ${s.pos}`);
-        s.destroy();
-      }
-    }
-  }
+  // Run Spawner
+  SpawnLogic.run();
 
-  // ALL OTHER SYSTEMS SUSPENDED
-  // RoomPlanner.run();
-  // SpawnLogic.run();
-  // ToolPixel.run();
+  // Run Pixel Generator
+  ToolPixel.run();
 
   // Run Creeps
-  /*
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     if (creep.memory.role === 'harvester') RoleHarvester.run(creep);
@@ -51,7 +35,6 @@ export const loop = () => {
     if (creep.memory.role === 'upgrader') RoleUpgrader.run(creep);
     if (creep.memory.role === 'worker') RoleWorker.run(creep);
   }
-  */
 
   // No CPU logs to keep the console clean
 };
