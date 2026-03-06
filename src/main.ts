2523b6c408045c973b46dc9fd11c2cb3338c5618
Hermann Hahn <hermann.h.hahn@gmail.com>
2026-03-06 19:03:42 -0300

    Main: fix syntax error in purification protocol

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import RoomScanner from "./rooms/room.scanner";
-import RoomPlanner from "./rooms/room.planner";
-import SpawnLogic from "./spawns/spawn.logic";
-import ToolPixel from "./tools/tool.pixel";
-import RoleHarvester from "./roles/role.harvester";
-import RoleSupplier from "./roles/role.supplier";
-import RoleUpgrader from "./roles/role.upgrader";
-import RoleWorker from "./roles/role.worker";
+// import RoomPlanner from "./rooms/room.planner";
+// import SpawnLogic from "./spawns/spawn.logic";
+// import ToolPixel from "./tools/tool.pixel";
+// import RoleHarvester from "./roles/role.harvester";
+// import RoleSupplier from "./roles/role.supplier";
+// import RoleUpgrader from "./roles/role.upgrader";
+// import RoleWorker from "./roles/role.worker";
 
 export const loop = () => {
   // Clear dead creep memory
@@ -41,15 +41,17 @@ export const loop = () => {
   // RoomPlanner.run();
   // SpawnLogic.run();
   // ToolPixel.run();
-...
+
   // Run Creeps
-  /* for (const name in Game.creeps) {
+  /*
+  for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     if (creep.memory.role === 'harvester') RoleHarvester.run(creep);
     if (creep.memory.role === 'supplier') RoleSupplier.run(creep);
     if (creep.memory.role === 'upgrader') RoleUpgrader.run(creep);
     if (creep.memory.role === 'worker') RoleWorker.run(creep);
-  } */
+  }
+  */
 
   // No CPU logs to keep the console clean
 };
